008c0c50ce3d5087728a5847a1983fc0cd047f99
LeahDaniel <leah.daniel1997@gmail.com>
2021-11-15 15:57:07 -0600

    through chapter 7

---
+++ src/components/employees/EmployeeList.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react"
+
+export const EmployeeList = () => {
+    const [employees, changeEmployee] = useState([])
+    const [specialtiesList, updateSpecialtiesList] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/employees")
+                .then(res => res.json())
+                .then((data) => {
+                    changeEmployee(data)
+                })
+        },
+        []
+    )
+
+    useEffect(() => {
+        /*
+            1. Use .map() to get the specialty of each employee
+            2. Then update a state variable to be a comma-separated string
+                (e.g. "iPhone, Printers, ...")
+        */
+        const specialtiesStr = employees.map(employee => employee.specialty).join(", ")
+
+        updateSpecialtiesList(specialtiesStr)
+
+    }, [employees])
+
+    return (
+        <>
+            <div>
+                Specialties: {specialtiesList}
+            </div>
+            {
+                employees.map(
+                    (employee) => {
+                        return <p key={`employee--${employee.id}`}>{employee.name}</p>
+                    }
+                )
+            }
+        </>
+    )
+}
+
